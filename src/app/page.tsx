3519caac24de4e23afedaf8ade21d4a69dd3d796
Maleva Olga <formaleva@gmail.com>
2025-02-06 10:24:39 +0300

    add main page

---
+++ src/app/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Title from "@/components/ui/Title";
+
+import Editor from "@/components/features/editor/Editor";
+import NotificationSender from "@/components/features/notification/NotificationSender";
+import Output from "@/components/features/output/Output";
+import Header from "@/components/layout/Header";
+import FlexContainer from "@/components/ui/FlexContainer";
+import Section from "@/components/ui/Section";
+import Wrapper from "@/components/ui/Wrapper";
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <Header>
+        <h1>Notification Json Generator</h1>
+      </Header>
+
+      <FlexContainer $wrap="wrap" $gap="2rem">
+        <Section>
+          <Title $marginBottom="2rem">Notification API Configurator</Title>
+          <Editor />
+        </Section>
+        <Section>
+          <Title $marginBottom="2rem">Output</Title>
+          <Output />
+          <NotificationSender $margin="1rem 0 0 0" />
+        </Section>
+      </FlexContainer>
+    </Wrapper>
+  );
+}
